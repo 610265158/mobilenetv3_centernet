4cd436cb4f740a6089a8509b4488c36bdf2a8292
liangzi <liangzi@staff.weibo.com>
2020-04-08 19:35:40 +0800

    change focal loss

--- lib/core/model/loss/centernet_loss.py
+++ lib/core/model/loss/centernet_loss.py
@@ -37,10 +37,7 @@ def loss(reg_predict,cls_predict,reg_label,cls_label_,num_gt):
     # reg_loss = tf.reduce_sum(location_losses) / normalizer
     # cla_loss = tf.reduce_sum(cls_losses)/normalizer
 
-    return location_losses,cls_losses/tf.reduce_sum(num_gt)
-
-
-
+    return location_losses,cls_losses
 
 
 def classification_loss(predictions, targets):
@@ -88,45 +85,69 @@ def localization_loss(predictions, targets,sigma=9):
     return  tf.reduce_sum(tf.where(abs_diff_lt_1, 0.5 * tf.square(abs_diff), abs_diff - 0.5/sigma))/normalizer
 
 
-def focal_loss(prediction_tensor, target_tensor, weights=None, alpha=0.25, gamma=2):
-    r"""Compute focal loss for predictions.
-        Multi-labels Focal loss formula:
-            FL = -alpha * (z-p)^gamma * log(p) -(1-alpha) * p^gamma * log(1-p)
-                 ,which alpha = 0.25, gamma = 2, p = sigmoid(x), z = target_tensor.
-    Args:
-     prediction_tensor: A float tensor of shape [batch_size, num_anchors,
-        num_classes] representing the predicted logits for each class
-     target_tensor: A float tensor of shape [batch_size, num_anchors,
-        num_classes] representing one-hot encoded classification targets
-     weights: A float tensor of shape [batch_size, num_anchors]
-     alpha: A scalar tensor for focal loss alpha hyper-parameter
-     gamma: A scalar tensor for focal loss gamma hyper-parameter
-    Returns:
-        loss: A (scalar) tensor representing the value of the loss function
-    """
+# def focal_loss(prediction_tensor, target_tensor, weights=None, alpha=0.25, gamma=2):
+#     r"""Compute focal loss for predictions.
+#         Multi-labels Focal loss formula:
+#             FL = -alpha * (z-p)^gamma * log(p) -(1-alpha) * p^gamma * log(1-p)
+#                  ,which alpha = 0.25, gamma = 2, p = sigmoid(x), z = target_tensor.
+#     Args:
+#      prediction_tensor: A float tensor of shape [batch_size, num_anchors,
+#         num_classes] representing the predicted logits for each class
+#      target_tensor: A float tensor of shape [batch_size, num_anchors,
+#         num_classes] representing one-hot encoded classification targets
+#      weights: A float tensor of shape [batch_size, num_anchors]
+#      alpha: A scalar tensor for focal loss alpha hyper-parameter
+#      gamma: A scalar tensor for focal loss gamma hyper-parameter
+#     Returns:
+#         loss: A (scalar) tensor representing the value of the loss function
+#     """
+#
+#
+#     sigmoid_p = tf.nn.sigmoid(prediction_tensor)
+#     zeros = array_ops.zeros_like(sigmoid_p, dtype=sigmoid_p.dtype)
+#
+#     # For poitive prediction, only need consider front part loss, back part is 0;
+#     # target_tensor > zeros <=> z=1, so poitive coefficient = z - p.
+#     pos_p_sub = array_ops.where(target_tensor > zeros, target_tensor - sigmoid_p, zeros)
+#
+#     # For negative prediction, only need consider back part loss, front part is 0;
+#     # target_tensor > zeros <=> z=1, so negative coefficient = 0.
+#     neg_p_sub = array_ops.where(target_tensor > zeros, zeros, sigmoid_p)
+#     per_entry_cross_ent = - alpha * (pos_p_sub ** gamma) * tf.log(tf.clip_by_value(sigmoid_p, 1e-8, 1.0)) \
+#                           - (1 - alpha) * (neg_p_sub ** gamma) * tf.log(tf.clip_by_value(1.0 - sigmoid_p, 1e-8, 1.0))
+#
+#
+#     # compute the normalizer: the number of positive anchors
+#     # normalizer = tf.where(tf.greater(target_tensor, 0))
+#     # normalizer = tf.cast(tf.shape(normalizer)[0], tf.float32)
+#     # normalizer = tf.maximum(1., normalizer)
+#
+#
+#     return tf.reduce_sum(per_entry_cross_ent)
 
 
-    sigmoid_p = tf.nn.sigmoid(prediction_tensor)
-    zeros = array_ops.zeros_like(sigmoid_p, dtype=sigmoid_p.dtype)
+def focal_loss(pred, gt):
+    ''' Modified focal loss. Exactly the same as CornerNet.
+        Runs faster and costs a little bit more memory
+      Arguments:
+        pred (batch,h,w,c)
+        gt_regr (batch,h,w,c)
+    '''
+    pos_inds = tf.cast(tf.equal(gt, 1.0), dtype=tf.float32)
+    neg_inds = 1.0 - pos_inds
+    neg_weights = tf.pow(1.0 - gt, 4.0)
 
-    # For poitive prediction, only need consider front part loss, back part is 0;
-    # target_tensor > zeros <=> z=1, so poitive coefficient = z - p.
-    pos_p_sub = array_ops.where(target_tensor > zeros, target_tensor - sigmoid_p, zeros)
+    pred = tf.clip_by_value(pred, 1e-6, 1.0 - 1e-6)
+    pos_loss = tf.log(pred) * tf.pow(1.0 - pred, 2.0) * pos_inds
+    neg_loss = tf.log(1.0 - pred) * tf.pow(pred, 2.0) * neg_weights * neg_inds
 
-    # For negative prediction, only need consider back part loss, front part is 0;
-    # target_tensor > zeros <=> z=1, so negative coefficient = 0.
-    neg_p_sub = array_ops.where(target_tensor > zeros, zeros, sigmoid_p)
-    per_entry_cross_ent = - alpha * (pos_p_sub ** gamma) * tf.log(tf.clip_by_value(sigmoid_p, 1e-8, 1.0)) \
-                          - (1 - alpha) * (neg_p_sub ** gamma) * tf.log(tf.clip_by_value(1.0 - sigmoid_p, 1e-8, 1.0))
+    num_pos = tf.reduce_sum(pos_inds)
+    pos_loss = tf.reduce_sum(pos_loss)
+    neg_loss = tf.reduce_sum(neg_loss)
 
+    loss = - (pos_loss + neg_loss) / num_pos
 
-    # compute the normalizer: the number of positive anchors
-    # normalizer = tf.where(tf.greater(target_tensor, 0))
-    # normalizer = tf.cast(tf.shape(normalizer)[0], tf.float32)
-    # normalizer = tf.maximum(1., normalizer)
-
-
-    return tf.reduce_sum(per_entry_cross_ent)
+    return loss
 
 
 def ohem_loss(logits, targets, weights):
