6bc44555acff89c579bd48faa72d8c70b44db9e3
liangzi <liangzi@staff.weibo.com>
2020-04-10 13:13:08 +0800

    change head;

--- lib/core/model/head/centernet_head.py
+++ lib/core/model/head/centernet_head.py
@@ -22,40 +22,34 @@ class CenternetHead():
         with slim.arg_scope(arg_scope):
             with tf.variable_scope('CenternetHead'):
                 c3, c4, c5 = fms
+                deconv_feature=c5
+                for i in range(3):
+                    deconv_feature=slim.conv2d_transpose(deconv_feature,num_outputs=256,kernel_size=(4,4),stride=2,padding='SAME')
 
-
-                p5_upsampled = self._upsample(c5,scope='upsample1')
-                # p4_upsampled = self._upsample(p5_upsampled, scope='upsample2')
-                # p2_feature = self._upsample(p4_upsampled, scope='upsample3')
-
-                p4 = tf.concat([c4,p5_upsampled],axis=3)
-                p4 = slim.conv2d(p4, 256, [3, 3], padding='SAME', scope='P4_after')
-
-                p3_upsampled = self._upsample(p4,scope='upsample_p4')
-
-                p3 = tf.concat([c3,p3_upsampled],axis=3)
-                p3 = slim.conv2d(p3, 128, [3, 3], padding='SAME', scope='P3_after')
-
-                p2_upsampled = self._upsample(p3,scope='upsample_p3')
-
-                p2_feature = slim.conv2d(p2_upsampled, 128, [3, 3], padding='SAME', scope='p2_feature')
-
-
-            size = slim.conv2d(p2_feature,
-                                      2,
-                                      [3, 3],
-                                      stride=1,
-                                      activation_fn=None,
-                                      normalizer_fn=None,
-                                      scope='centernet_reg_output')
-
-            kps = slim.conv2d(p2_feature,
-                                cfg.DATA.num_class,
-                                [3, 3],
-                                stride=1,
-                                activation_fn=None,
-                                normalizer_fn=None,
-                                scope='centernet_cls_output')
+                size = slim.conv2d(deconv_feature,
+                                   128,
+                                   [3, 3],
+                                   stride=1,
+                                   scope='centernet_reg_output')
+                size = slim.conv2d(size,
+                                  2,
+                                  [3, 3],
+                                  stride=1,
+                                  activation_fn=tf.nn.relu,
+                                  normalizer_fn=None,
+                                  scope='centernet_reg_output')
+                kps = slim.conv2d(deconv_feature,
+                                  128,
+                                  [3, 3],
+                                  stride=1,
+                                  scope='centernet_cls_output')
+                kps = slim.conv2d(kps,
+                                    cfg.DATA.num_class,
+                                    [3, 3],
+                                    stride=1,
+                                    activation_fn=tf.nn.sigmoid,
+                                    normalizer_fn=None,
+                                    scope='centernet_cls_output')
 
         return size,kps
 
