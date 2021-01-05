ed41e6c35647bace0698ae728dc047511e12bb7a
liangzi <liangzi@staff.sina.com.cn>
2021-01-05 15:26:39 +0800

    add opencv thread control

--- train.py
+++ train.py
@@ -2,7 +2,9 @@ from lib.helper.logger import logger
 from lib.core.base_trainer.net_work import trainner
 import setproctitle
 
-
+import cv2
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
 
 logger.info('train start')
 setproctitle.setproctitle("detect")
